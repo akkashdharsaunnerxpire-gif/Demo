6fe4d0a4a9aea2ad4d76557b7ca6e1649a24bb44
AkkashDharsaun <akkashdharsaun02@gmail.com>
2026-03-25 15:33:30 +0530

    update hello.php

--- hello.php
+++ hello.php
@@ -1,8 +1,21 @@
-<?php
-$a = "";
-for($i=1; $i<=6; $i++){
-    $a = $a . "*";
-    echo "*";
-    echo "</br>";
-}
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+    <h2>Login Page</h2>
+
+    <form>
+        <label>Email:</label><br>
+        <input type="email" name="email"><br><br>
+
+        <label>Password:</label><br>
+        <input type="password" name="password"><br><br>
+
+        <button type="submit">Login</button>
+    </form>
+
+</body>
+</html>
